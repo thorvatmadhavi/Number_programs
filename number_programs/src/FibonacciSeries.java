bf5f4eabc5991eebccd09e5ada8fc32cc99d1611
thorvatmadhavi <thorvatmadhavi@gmail.com>
2025-05-02 17:35:33 +0530

    Add program to get Fibonacci series

---
+++ number_programs/src/FibonacciSeries.java
@@ -0,0 +1,17 @@
+public class FibonacciSeries {
+    public static void main(String args[])
+    {
+        int a = 0, b = 1, c  =0;
+        int termsNo =10;
+
+        System.out.print(a + " " + b);  // printing 0 and 1
+
+        for(int i = 2; i < termsNo; ++i)   // loop starts from 2 because 0 and 1 are already printed
+        {
+            c = a + b;
+            System.out.print(" " + c);
+            a = b;
+            b = c;
+        }
+    }
+}
